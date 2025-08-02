66a0506fee60a0c27ea8700d597a37c79f537a65
Soham-Mane <sohamnmane03@gmail.com>
2025-08-02 12:24:25 +0530

    Initial commit for client and server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+require('dotenv').config();
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
